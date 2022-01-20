28151f2e1e767167c6b3e26465490c8f299173cb
François Michaud <fmichaud@deepnox.io>
2022-01-20 14:14:18 +0100

    Add missing templates

--- src/loadguard/templates/__init__.py
+++ src/loadguard/templates/__init__.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 
 """
-This module provides tools to manage stores.
+This module provides templates utilities.
 
-Package: :module:`loadguard.stores`
+Package: :module:`loadguard.templates`
 
 This file is a part of LoadGuard Runner.
 
 (c) 2021, Deepnox SAS.
 
-
 """
 
 __import__("pkg_resources").declare_namespace(__name__)
-
