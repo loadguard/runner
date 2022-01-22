f757731a61b8b8582bbadd410f88c70839ff961a
François Michaud <fmichaud@deepnox.io>
2022-01-22 16:39:46 +0100

    Fix bad packages names

--- src/loadguard/plugins/__init__.py
+++ src/loadguard/plugins/__init__.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
+
 """
-# loadguard.tests.core.plugins
+Package: :module:`loadguard.plugins.__init__`
 
 This file is a part of LoadGuard Runner.
 
 (c) 2021, Deepnox SAS.
 
-This module provides utilities to register and use LoadGuard Runner plugins.
 
 """
+
+__import__("pkg_resources").declare_namespace(__name__)
+
--- src/loadguard/runner/__init__.py
+++ src/loadguard/runner/__init__.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
-"""
-# loadguard.runner
-
-This module provides app runner to execute a LoadGuard project.
 
 """
+Package: :module:`loadguard.runners.__init__`
+
+This file is a part of LoadGuard Runner.
+
+(c) 2021, Deepnox SAS.
+
+
+"""
+
+__import__("pkg_resources").declare_namespace(__name__)
+
 
 import importlib
 import inspect
