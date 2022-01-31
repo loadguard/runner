967f4fe17abb62d5601cd9ae27b5b830dfd831e2
François Michaud <fmichaud@deepnox.io>
2022-01-31 19:17:23 +0100

    Request replay

---
+++ test/loadguard/tests/scenarii/replay_test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""
+Unit tests for module: loadguard.tests.scenarii.base_test
+
+This file is a part of LoadGuard project.
+
+(c) 2021, Deepnox SAS.
+"""
+
+import unittest
+
+from deepnox.helpers.testing_helpers import BaseAsyncTestCase
+from loadguard.scenarii.replay import HttpRequestReplay
+
+
+class HttpRequestReplayTestCase(BaseAsyncTestCase):
+    """
+    Test case for base scenario.
+    """
+
+    def test__create_a_valid_instance(self):
+        """
+        Tests: create a new instance of scenario.
+        """
+        self.assertIsInstance(HttpRequestReplay(self.loop), HttpRequestReplay)
+
+    def test__create_a_valid_instance_should_raise_an_error(self):
+        """
+        Tests: try to create an instance using invalid parameters.
+        """
+        self.assertRaises(TypeError, lambda: HttpRequestReplay(self.loop))
+
+
+if __name__ == '__main__':
+    unittest.main()
